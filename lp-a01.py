79bfce68223731dd84e2b8b29b1825f765d06a58
fabio-falc@hotmail.com <fabio-falc@hotmail.com>
2025-01-26 16:41:55 -0300

    adiciona arquivo LP-A01.py

---
+++ lp-a01.py
@@ -0,0 +1,19 @@
+
+
+nome = "fabio"
+idade = 32
+altura = 1.67
+is_estudante = True
+
+
+print("Exemplo de variáveis:\n")
+print(f"Nome (String): {nome}")
+print(f"Idade (Integer): {idade}")
+print(f"Altura (Float): {altura}")
+print(f"É estudante? (Boolean): {is_estudante}\n")
+
+
+first_code = input("Digite o primeiro conceito universal na programação: ")
+
+
+print(f" Que incrivel {nome},   ' {first_code} ' , É a primeira frase que aprendemos a programar e imprimir.")
