e757ed5fe46f7b399fa9f05e7e1217fb0c1d5c0a
fabio-falc@hotmail.com <fabio-falc@hotmail.com>
2025-03-29 19:31:42 -0300

    LPIA-A01

---
+++ lpia-a01.py
@@ -0,0 +1,15 @@
+
+# [LPIA-A01] Você está criando um programa para calcular a área de um retângulo. O programa deve solicitar ao usuário que forneça a base e a altura do retângulo. Em seguida, o programa deve calcular a área usando a fórmula:
+
+# Área=Base×Altura
+
+
+print('--- Calculo de Área ---')
+
+base = float(input('Insira a base do retângulo: '))
+
+altura = float(input('Insira a altura do retângulo: '))
+
+area = base * altura 
+
+print(f'A área do retângulo é: {area}')
