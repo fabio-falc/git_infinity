44b869b7a5174ee1934886ed5f44b18ece7d3347
fabio-falc@hotmail.com <fabio-falc@hotmail.com>
2025-05-30 22:38:46 -0300

    prova listas e tuplas

---
+++ pyia-a01.py
@@ -0,0 +1,12 @@
+
+# Lista de Frutas
+
+frutas_lista = ["laranja", "abacaxi", "melancia", "uva", "maracuja","tamarindo"]
+
+# Convertendo a lista em uma tupla
+
+frutas_tupla = tuple(frutas_lista)
+
+# Exibindo o conteúdo da tupla
+
+print(frutas_tupla)
