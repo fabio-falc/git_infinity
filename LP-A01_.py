213bca201ce20321acf29e32f6c5161edb35e0ad
fabio-falc@hotmail.com <fabio-falc@hotmail.com>
2025-07-07 05:43:09 -0300

    exemplo com diversas tipagens

---
+++ LP-A01_.py
@@ -0,0 +1,22 @@
+# Exemplos de variáveis com diferentes tipos
+
+nome = "Fabio" # string
+idade = 33  # int
+altura = 1.67   # float
+is_estudante = True #Booleano
+
+# Exibição de tipos de dados
+
+print("=== Exemplos de variáveis com tipagens diferentes ===\n")
+print(f"Nome (String): {nome}")
+print(f"Idade (Integer): {idade}")
+print(f"Altura (Float): {altura}")
+print(f"Is_estudante (Boolean): {is_estudante}\n")
+
+# Solicitação de entrada de usuário
+
+first_code = input("Digite o primeiro conceito universal na Programação: ")
+
+# Exibição
+
+print(f"\nMuito bem, {nome}, '{first_code}' é realmente uma das primeiras frases que aprendemos a programar.")
